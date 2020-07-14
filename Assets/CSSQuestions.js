bfff73e76ab6fe1e1df9fe69779377029a277e9c
christopher@zarlengo.net <christopher@zarlengo.net>
2020-07-14 12:08:50 -0700

    Added more quiz questions

---
+++ Assets/CSSQuestions.js
@@ -0,0 +1,189 @@
+var CSSquestions = [
+	{
+		title:"Question 1",
+		question:"If we want to define the style for an unique element, then which css selector will we use?",
+		answers:{
+			a:"Id",
+			b:"text",
+			c:"class",
+			d:"name"
+		},
+		answer:"a"
+	},
+	{
+		title:"Question 2",
+		question:"If we don't want to allow a floating div to the left side of an element, which css property will we use?",
+		answers:{
+			a:"margin",
+			b:"clear",
+			c:"float",
+			d:"padding"
+		},
+		answer:"b"
+	},
+	{
+		title:"Question 3",
+		question:"Suppose we want to arrange five DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
+		answers:{
+			a:"d-index",
+			b:"s-index",
+			c:"x-index",
+			d:"z-index"
+		},
+		answer:"d"
+	},
+	{
+		title:"Question 4",
+		question:"If we want to wrap a block of text around an image, which css property will we use?",
+		answers:{
+			a:"wrap",
+			b:"push",
+			c:"float",
+			d:"align"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 5",
+		question:"If we want to show an Arrow as cursor, then which value we will use?",
+		answers:{
+			a:"pointer",
+			b:"default",
+			c:"arrow",
+			d:"arr"
+		},
+		answer:"b"
+	},
+	{
+		title:"Question 6",
+		question:"If we want to use a nice looking green dotted border around an image, which css property will we use?",
+		answers:{
+			a:"border-color",
+			b:"border-decoration",
+			c:"border-style",
+			d:"border-line"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 7",
+		question:"Which of the following properties will we use to display border around a cell without any content?",
+		answers:{
+			a:"empty-cell",
+			b:"blank-cell",
+			c:"noncontent-cell",
+			d:"void-cell"
+		},
+		answer:"a"
+	},
+	{
+		title:"Question 8",
+		question:"What should be the table width, so that the width of a table adjust to the current width of the browser window?",
+		answers:{
+			a:"640 pixels",
+			b:"100%",
+			c:"full-screen",
+			d:"1024 px"
+		},
+		answer:"b"
+	},
+	{
+		title:"Question 9",
+		question:"Which element is used in the <HEAD> section on an HTML / XHTMLpage, if we want to use an external style sheet file to decorate the page?",
+		answers:{
+			a:"<src>",
+			b:"<link>",
+			c:"<style>",
+			d:"<css>"
+		},
+		answer:"b"
+	},
+	{
+		title:"Question 10",
+		question:"Which attribute can be added to many HTML / XHTML elements to identify them as a member of a specific group?",
+		answers:{
+			a:"Id",
+			b:"div",
+			c:"class",
+			d:"span"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 11",
+		question:`When we write <img src="img.png">, what "img.png" inside double quote implies?`,
+		answers:{
+			a:"element",
+			b:"attribute",
+			c:"value",
+			d:"operator"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 12",
+		question:"How can we write comment along with CSS code?",
+		answers:{
+			a:"/* a comment */",
+			b:"// a comment //",
+			c:"/ a comment /",
+			d:"<' a comment'>"
+		},
+		answer:"a"
+	},
+	{
+		title:"Question 13",
+		question:"Which css property you will use if you want to add some margin between a DIV's border and its inner text?",
+		answers:{
+			a:"spacing",
+			b:"margin",
+			c:"padding",
+			d:"inner-margin"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 14",
+		question:"Which CSS property is used to control the text size of an element?",
+		answers:{
+			a:"font-style",
+			b:"text-size",
+			c:"font-size",
+			d:"text-style"
+		},
+		answer:"c"
+	},
+	{
+		title:"Question 15",
+		question:`The default value of "position" attribute is ___.`,
+		answers:{
+			a:"fixed",
+			b:"absolute",
+			c:"inherit",
+			d:"relative"
+		},
+		answer:"d"
+	},
+	{
+		title:"Question 16",
+		question:"How will you make all paragraph elements 'RED' in color?",
+		answers:{
+			a:"p.all {color: red;}",
+			b:"p.all {color: #990000;}",
+			c:"all.p {color: #998877;}",
+			d:"p {color: red;}"
+		},
+		answer:"d"
+	},
+	{
+		title:"Question 17",
+		question:"By default Hyperlinks are displayed with an underline. How do you remove the underline from all hyperlinks by using CSS code?",
+		answers:{
+			a:"a {text: no-underline;}",
+			b:"a {text-decoration:none;}",
+			c:"a {text-style: no-underline;}",
+			d:"a {text-decoration: no-underline;}"
+		},
+		answer:"a"
+	}
+];
